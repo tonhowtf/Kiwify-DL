0f6eb090383d5247eb1c6383f4a6442fe65d5d5e
Tonho <antonioreverso.com@gmail.com>
2025-03-07 11:47:15 -0300

    Add debug function to load and process JSON data for stream links

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ def stream_links(data):
     for item in data:
       stream_links(item)
 
+def debug(json):
+  with open(json, 'r', encoding='utf-8') as file:
+    data = json.load(file)
+    stream_links(data)
 
-
+debug('data.json')
 
