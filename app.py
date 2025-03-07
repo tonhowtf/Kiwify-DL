cfa81959f34675d40c83d8a823110bc599816076
Tonho <antonioreverso.com@gmail.com>
2025-03-07 19:29:33 -0300

    Refactor stream_links function to accumulate stream links in a list and return it

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
 import json
 
+links = []
+
 def stream_links(data):
   if isinstance(data, dict):
     for key, value in data.items():
       if key == 'stream_link':
-        print(value)
+        links.append(value)
       else:
-        stream_links(value)
+        links.extend(stream_links(value))
   elif isinstance(data, list):
     for item in data:
-      stream_links(item)
+      links.extend(stream_links(item))
+  return links
 
-def debug(file):
-  with open(file, 'r', encoding='utf-8') as file:
-    data = json.load(file)
-    stream_links(data)
 
-debug('data.json')
+
 
