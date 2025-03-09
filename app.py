1e137ed0cb6e0ed186101bbed07730b542329d11
Tonho <antonioreverso.com@gmail.com>
2025-03-09 03:14:05 -0300

    Replace stream_links function with json function to load JSON data from a file

--- app.py
+++ app.py
@@ -1,18 +1,8 @@
 import json
 
-links = []
-
-def stream_links(data):
-  if isinstance(data, dict):
-    for key, value in data.items():
-      if key == 'stream_link':
-        links.append(value)
-      else:
-        links.extend(stream_links(value))
-  elif isinstance(data, list):
-    for item in data:
-      links.extend(stream_links(item))
-  return links
+def json(data):
+  with open(filename, 'r', encoding='utf-8') as f:
+    return json.load(f)
 
 
 
